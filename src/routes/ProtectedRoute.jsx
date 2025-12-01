66dd03e645648d416abbc73275552991a980f922
PYONIUMM <ivapanto97@gmail.com>
2025-12-01 02:49:50 +0100

    redirect issue

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -54,9 +54,5 @@ export default function RequireAuth() {
     return <Navigate to="/onboarding" replace state={{ from: location }} />;
   }
 
-  if (!needsOnboarding && isOnboardingRoute) {
-    return <Navigate to="/app" replace />;
-  }
-
   return <Outlet />;
 }
